574832dfd31de1377783f599979aaecec69a4288
Cheyenne Atapour <CheyenneAtapour@gmail.com>
2021-09-26 21:15:19 -0700

    add purchase token function

--- contracts/erchouse.sol
+++ contracts/erchouse.sol
@@ -25,6 +25,7 @@ contract ERCHouse is AccessControl
 	mapping(address => bool) public tokenWhitelist;
 
 	bytes32 public constant TOKEN_MANAGER = keccak256('TOKEN_MANAGER');
+	bytes32 public constant FUNDS_MANAGER = keccak256('FUNDS_MANAGER');
 
 	event tokenListed(address seller, ERC20 sellToken, ERC20 buyToken, uint256 amount, uint256 price, uint256 indexed listingId);
 	event tokenPurchased(address seller, ERC20 sellToken, ERC20 buyToken, uint256 amount, uint256 price, uint256 indexed listingId);
@@ -45,6 +46,11 @@ contract ERCHouse is AccessControl
 		tokenWhitelist[token] = true;
 	}
 
+	function collectRegistrationFees() public onlyRole(FUNDS_MANAGER)
+	{
+		payable(msg.sender).transfer(address(this).balance);
+	}
+
 	// Adds a token to the token whitelist
 	function addToken(address token) public onlyRole(TOKEN_MANAGER)
 	{
@@ -60,6 +66,7 @@ contract ERCHouse is AccessControl
 	}
 
 	// Allows seller of a token to create a listing
+	// pricePerToken is price of 1 unit of sellToken expressed in units of buyToken
 	function listToken(address sellToken, address buyToken, uint256 amountToSell, uint256 pricePerToken) public
 	{
 		require(tokenWhitelist[sellToken] && tokenWhitelist[buyToken], "Both sell and buy tokens must be whitelisted");
@@ -73,16 +80,25 @@ contract ERCHouse is AccessControl
 	// Allows seller of a token to cancel a listing
 	function cancelListing(uint256 listingId) public
 	{
-		// Give amount of sellToken back to seller
 		TokenSale memory listing = listedTokens[listingId];
-		sellToken.transfer(listing.seller, listing.amount);
+		require(msg.sender == listing.seller, "Caller must be the original creator of listing");
+		// Give amount of sellToken back to seller
+		listing.sellToken.transfer(listing.seller, listing.amount);
 		// Set the listing amount to 0
 		listedTokens[listingId].amount = 0;
 	}
 
 	// Allows a buyer to purchase part or all of a token listing
-	function purchaseToken() public
+	function purchaseToken(uint256 listingId, uint256 amountToBuy) public
 	{
+		TokenSale memory listing = listedTokens[listingId];
+		require(amountToBuy <= listing.amount, "Purchase amount cannot exceed listing amount");
 
+		// Buyer pays lister for the tokens
+		listing.buyToken.transferFrom(msg.sender, listing.seller, amountToBuy * listing.price);
+		// Buyer receives tokens from contract
+		listing.sellToken.transfer(msg.sender, amountToBuy);
+		// Listing amount is decreased by amount bought
+		listedTokens[listingId].amount -= amountToBuy;
 	}
 }
