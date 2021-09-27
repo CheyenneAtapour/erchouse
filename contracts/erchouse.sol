95cb2207ce41c2fa92a0c74204e785cb700d1ff1
Cheyenne Atapour <CheyenneAtapour@gmail.com>
2021-09-26 18:06:59 -0700

    add code skeleton

---
+++ contracts/erchouse.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: Unlicense
+
+pragma solidity 0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract ERCHouse is AccessControl
+{
+	address payable superAdmin;
+
+	struct TokenSale{
+		address payable seller;
+		ERC20 sellToken;
+		ERC20 buyToken;
+		uint256 amount;
+		uint256 price;
+	}
+
+	uint256 listings;
+	mapping(uint256 => TokenSale) public listedTokens;
+	mapping(address => bool) public tokenWhitelist;
+
+	bytes32 public constant TOKEN_MANAGER = keccak256('TOKEN_MANAGER');
+
+	event tokenListed(address seller, ERC20 sellToken, ERC20 buyToken, uint256 amount, uint256 price, uint256 indexed listingId);
+	event tokenPurchased(address seller, ERC20 sellToken, ERC20 buyToken, uint256 amount, uint256 price, uint256 indexed listingId);
+
+	constructor(address payable _superAdmin)
+	{
+		listings = 0;
+		_setupRole(DEFAULT_ADMIN_ROLE, _superAdmin);
+		superAdmin = _superAdmin;
+	}
+
+	// Adds a token to the token whitelist
+	function addToken() public onlyRole(TOKEN_MANAGER)
+	{
+
+	}
+
+	// Removes a token from the token whitelist
+	function removeToken() public onlyRole(TOKEN_MANAGER)
+	{
+
+	}
+
+	// Allows seller of a token to create a listing
+	function listToken() public
+	{
+
+	}
+
+	// Allows seller of a token to cancel a listing
+	function cancelListing() public
+	{
+
+	}
+
+	// Allows a buyer to purchase part or all of a token listing
+	function purchaseToken() public
+	{
+
+	}
+}
